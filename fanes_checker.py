955f79367cc5d6ec20c9c31baa982fb1aab38d1c
MalaPetra <39831897+MalaPetra@users.noreply.github.com>
2026-05-24 21:18:12 +0100

    Update fanes_checker.py

--- fanes_checker.py
+++ fanes_checker.py
@@ -14,10 +14,14 @@ HEADERS = {
     "User-Agent": (
         "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) "
         "AppleWebKit/537.36 (KHTML, like Gecko) "
-        "Chrome/121.0 Safari/537.36"
+        "Chrome/121.0.0.0 Safari/537.36"
     ),
     "Origin": "https://www.rifugiofanes.com",
-    "Referer": "https://www.rifugiofanes.com/",
+    "Referer": "https://www.rifugiofanes.com/en/booking",
+    "Accept": "application/json, text/plain, */*",
+    "Accept-Language": "en-GB,en;q=0.9",
+    "Accept-Encoding": "gzip, deflate, br",
+    "Connection": "keep-alive",
 }
 
 
